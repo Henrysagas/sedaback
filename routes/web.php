2da9c54ebabae40fd7d8af4096a4b7d618496189
Henrysagas <Henrybalta@hotmail.com>
2025-05-29 22:42:03 -0500

    Todo a web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Controladores API
+use App\Http\Controllers\Api\RolController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ZonaController;
+use App\Http\Controllers\Api\CorteController;
+use App\Http\Controllers\Api\EvidenciaController;
+use App\Http\Controllers\Api\HistorialController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,5 +25,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Rutas API RESTful
+Route::apiResource('roles', RolController::class);
+Route::apiResource('usuarios', UserController::class);
+Route::apiResource('zonas', ZonaController::class);
+Route::apiResource('cortes', CorteController::class);
+Route::apiResource('historiales', HistorialController::class);
+
+// Rutas personalizadas para Evidencias
+Route::get('evidencias', [EvidenciaController::class, 'index']);
+Route::post('evidencias', [EvidenciaController::class, 'store']);
+Route::get('evidencias/{evidencia}', [EvidenciaController::class, 'show']);
+Route::put('evidencias/{evidencia}', [EvidenciaController::class, 'update']);
+Route::delete('evidencias/{evidencia}', [EvidenciaController::class, 'destroy']);
+Route::post('subir-evidencia', [EvidenciaController::class, 'subirEvidencia']);
 
 require __DIR__.'/auth.php';
