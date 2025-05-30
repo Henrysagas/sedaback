b1b78e4e11d5b57341071e4c7b92e3466d2da444
Henrysagas <Henrybalta@hotmail.com>
2025-05-29 22:29:42 -0500

    migraciones arregladas

--- database/migrations/2025_05_24_235259_create_users_table.php
+++ database/migrations/2025_05_24_235259_create_users_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->foreignId('rol_id')->constrained('roles')->onDelete('cascade');
+            $table->foreignId('rol_id')->constrained('rols')->onDelete('cascade');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
